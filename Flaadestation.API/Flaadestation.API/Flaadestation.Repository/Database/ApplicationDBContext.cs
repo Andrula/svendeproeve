aac8b3f48310eb9719e859d99866afe3d58aa785
Nicklas El Beik <nick719h@elev.tec.dk>
2025-08-14 12:45:54 +0200

    Sat DB Context og database op

---
+++ Flaadestation.API/Flaadestation.API/Flaadestation.Repository/Database/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flaadestation.Repository.Database
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
