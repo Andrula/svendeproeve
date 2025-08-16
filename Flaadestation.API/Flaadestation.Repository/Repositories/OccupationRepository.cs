fe3b52f1927de9f3fcefef4f4fe3a64c3a862837
Andreas <xandreas@live.dk>
2025-08-16 18:56:12 +0200

    Tilføjet manglende repository og interface til Occupation entiteten

---
+++ Flaadestation.API/Flaadestation.Repository/Repositories/OccupationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Flaadestation.Repository.Database;
+using Flaadestation.Repository.Database.Entities;
+using Flaadestation.Repository.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flaadestation.Repository.Repositories
+{
+    public class OccupationRepository : Repository<Occupation>, IOccupationRepository
+    {
+        public OccupationRepository(ApplicationDBContext dbContext) : base(dbContext)
+        {
+            
+        }
+    }
+}
