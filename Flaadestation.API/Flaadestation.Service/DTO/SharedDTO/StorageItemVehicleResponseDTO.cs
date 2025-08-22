944267e11bae14f08e684c54922b23a89d7232bc
Nicklas El Beik <nick719h@elev.tec.dk>
2025-08-22 14:16:21 +0200

    Job Response færdigt

--- Flaadestation.API/Flaadestation.Service/DTO/SharedDTO/StorageItemVehicleResponseDTO.cs
+++ Flaadestation.API/Flaadestation.Service/DTO/SharedDTO/StorageItemVehicleResponseDTO.cs
@@ -17,14 +17,15 @@ namespace Flaadestation.Service.DTO.SharedDTO
         public string StorageItemNote { get; set; } = string.Empty;
         public string ItemNote { get; set; } = string.Empty;
         public Guid ImageId { get; set; }
-        public required byte[] ImageValue { get; set; }
+        public byte[]? ImageValue { get; set; }
 
         public List<StorageItemVehicleEmployeeResponseDTO> Employees { get; set; } = [];
-        public List<StorageItemVehicleToolResponseDTO>[] Tools { get; set; } = [];
+        public List<StorageItemVehicleToolResponseDTO> Tools { get; set; } = [];
     }
 
     public class StorageItemVehicleEmployeeResponseDTO
     {
+        public Guid ItemId { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
@@ -33,14 +34,15 @@ namespace Flaadestation.Service.DTO.SharedDTO
         public string Occupation { get; set; } = string.Empty;
         public string Note { get; set; } = string.Empty;
         public Guid ImageId { get; set; }
-        public required byte[] ImageValue { get; set; }
+        public byte[]? ImageValue { get; set; }
     }
 
     public class StorageItemVehicleToolResponseDTO
     {
+        public Guid ItemId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Note { get; set; } = string.Empty;
         public Guid ImageId { get; set; }
-        public required byte[] ImageValue { get; set; }
+        public byte[]? ImageValue { get; set; }
     }
 }
