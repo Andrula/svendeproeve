1aa755cfcfbed818b1838f72bfeadd1436f4679e
Andreas <xandreas@live.dk>
2025-08-28 16:02:43 +0200

    Ryddet op i program.cs, smidt services og reposiroty registreringer ind i en extension metode, så det ser clean ud og kan genbruges.

---
+++ Flaadestation.API/Flaadestation.ASP/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Flaadestation.Service.Interfaces;
+using Flaadestation.Service.Services;
+using System.Data;
+
+namespace Flaadestation.ASP.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICompanyService, CompanyService>();
+            services.AddScoped<IOccupationService, OccupationService>();
+            services.AddScoped<IBaseService, BaseService>();
+            services.AddScoped<IStorageService, StorageService>();
+            services.AddScoped<IStorageItemService, StorageItemService>();
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<IJobService, JobService>();
+            services.AddScoped<IToolService, ToolService>();
+            services.AddScoped<IVehicleService, VehicleService>();
+            services.AddScoped<IMachineryService, MachineryService>();
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            return services;
+        }
+    }
+}
