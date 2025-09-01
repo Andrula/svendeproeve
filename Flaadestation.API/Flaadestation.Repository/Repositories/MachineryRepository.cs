5bac90a0babe3cb94d727c62f800cec5dbdc9da0
Andreas <xandreas@live.dk>
2025-09-01 03:43:36 +0200

    Fikset logik for machinery repository, fikset fejl i seeding af data i application db context.

--- Flaadestation.API/Flaadestation.Repository/Repositories/MachineryRepository.cs
+++ Flaadestation.API/Flaadestation.Repository/Repositories/MachineryRepository.cs
@@ -24,9 +24,13 @@ namespace Flaadestation.Repository.Repositories
                     .ThenInclude(si => si.Storage)
                         .ThenInclude(s => s.Job)
                 .Include(t => t.DefaultStorage)
+                    .ThenInclude(ds => ds.Base)     
+                .Include(t => t.DefaultStorage)
+                    .ThenInclude(ds => ds.Job)      
                 .Where(m => m.CompanyId == companyId)
                 .ToListAsync();
         }
+
         public override async Task<Machinery?> GetByIdAsync(Guid id)
         {
             return await _dbSet
