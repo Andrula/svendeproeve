108e34a9dc5de77d46c751f9f27da9c96ccbb011
Andreas <xandreas@live.dk>
2025-09-07 20:58:46 +0200

    Fikset merge konflikter

--- Flaadestation.Dashboard/src/App.tsx
+++ Flaadestation.Dashboard/src/App.tsx
@@ -1,35 +1,139 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import Layout from "./Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmployeeComponent from "./Components/EmployeeComponent/EmployeeComponent";
+import VehicleComponent from "./Components/VehicleComponent/VehicleComponent";
+import MachineryComponent from "./Components/MachineryComponent/MachineryComponent";
+import ToolComponent from "./Components/ToolComponent/ToolComponent";
+import MapComponent from "./Components/MapComponent/MapComponent";
+import { ProtectedRoute } from "./Auth/ProtectedRoute";
+import HomeComponent from "./Components/HomeComponent/HomeComponent";
+import LoginComponent from "./Components/LoginComponent/LoginComponent";
+import RegisterComponent from "./Components/RegisterComponent/RegisterComponent";
+import CheckoutComponent from "./Components/CheckoutComponent/CheckoutComponent";
+import LicenseComponent from "./Components/LicenseComponent/LicenseComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <HomeComponent />
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <LoginComponent />
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <RegisterComponent />
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/register/checkout"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <CheckoutComponent />
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/employees"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <ProtectedRoute>
+                    <EmployeeComponent />
+                  </ProtectedRoute>
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/vehicles"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <ProtectedRoute>
+                    <VehicleComponent />
+                  </ProtectedRoute>
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/machines"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <ProtectedRoute>
+                    <MachineryComponent />
+                  </ProtectedRoute>
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/tools"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <ProtectedRoute>
+                    <ToolComponent />
+                  </ProtectedRoute>
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/licenses"
+            element={
+              <div className="position-relative p-4 h-100">
+                <div className="container">
+                  <ProtectedRoute>
+                    <LicenseComponent />
+                  </ProtectedRoute>
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/map"
+            element={
+              <div className="position-relative h-100" style={{ padding: 0 }}>
+                <ProtectedRoute>
+                  <MapComponent />
+                </ProtectedRoute>
+              </div>
+            }
+          />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ Flaadestation.Dashboard/src/Components/MapComponent/MapComponent.tsx
@@ -0,0 +1,59 @@
+import "./MapComponent.css";
+import { useEffect, useState } from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { JobModel } from "../../Models/Job";
+import { jobService } from "../../Services/JobService";
+import { dawaService } from "../../Services/DawaService";
+import { JobMarkerComponent } from "../JobMarkerComponent/JobMarkerComponent";
+import { Polygon } from "../MapComponent/Polygon";
+
+
+export default function GoogleMap() {
+  const [jobs, setJobs] = useState<JobModel[]>([]);
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string;
+
+  useEffect(() => {
+    loadJobs();
+  }, []);
+
+  const loadJobs = async () => {
+    const jobModels = await jobService.getJobsByCompany();
+    await dawaService.fillAddressesOnJobs(jobModels);
+    setJobs(jobModels);
+  };
+
+  const denmarkBounds = {
+    north: 57.75,
+    south: 54.56,
+    west: 8.08,
+    east: 15.16
+  }
+
+  return (
+    <div className="map-container">
+      <APIProvider apiKey={apiKey}>
+        <Map
+          restriction={{
+            latLngBounds: denmarkBounds,
+            strictBounds: false,
+          }}
+          mapId={"1"}
+          defaultCenter={{ lat: 56.26392, lng: 9.501785 }}
+          defaultZoom={6}
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+        >
+
+          {jobs.map((job) => (
+            <JobMarkerComponent
+              key={job.data.jobId}
+              job={job}
+            />
+          ))}
+        </Map>
+      </APIProvider>
+    </div>
+  );
+}
